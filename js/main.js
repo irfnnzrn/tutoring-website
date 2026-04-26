a5317dc65c2cb1955f4db991fba740eb9649f09f
irfnnzrn <m.irfan.nazran2@gmail.com>
2026-04-26 13:29:21 +0800

    Add files via upload
    template

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// =============================================
+//   LearnHub - Shared JavaScript
+//   Semua members boleh guna functions ni
+// =============================================
+
+// TODO: Tambah shared functions kat sini
+// Contoh: fetch data, render cards, dsb.
